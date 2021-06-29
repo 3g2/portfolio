43ab071f78b103d8fdbce27a15b7ebdf66262f2e
mohur <m.ashrahfi@gmail.com>
2021-06-29 00:01:22 -0700

    Add basic header component

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,21 @@
+import React from "react"
+import "./Header.scss"
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1 className="logo">Maher Halabi</h1>
+        <nav>
+          <ul>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+            <li>Link 4</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Header
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 import "../styles/index.scss"
+import Header from "../components/header/Header.js"
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Header />
+      <h1>Hello</h1>
+    </div>
+  )
 }
