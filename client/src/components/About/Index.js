8ebaa41dc26ce489d5af5c386d54736fa32500e0
mohur <m.ashrahfi@gmail.com>
2021-07-13 15:19:33 -0700

    Add useState to form data

--- client/src/components/About/Index.js
+++ client/src/components/About/Index.js
@@ -6,15 +6,16 @@ const Index = () => {
       <h1 class="header">About</h1>
       <div>
         <p class="span">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          At first, I focused on becoming an architectural engineer. The idea of
+          being able to develop and construct something that will someday be
+          made, used, and inhabited is exciting and inspires me to pursue such a
+          career. It was until I was first introduced to programming by a friend
+          who was pursuing a career in software development back in 2018. After
+          writing my first few lines and eventually getting my code to run
+          successfully after numerous tries, I finally saw the output of what I
+          have built. This sheer excitement I felt after discovering a new
+          element of creativity made me, later on, focus towards building my
+          skill set for programming ever since.
         </p>
       </div>
     </div>
--- client/src/components/Contact/Index.js
+++ client/src/components/Contact/Index.js
@@ -1,38 +1,89 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 const Index = () => {
   const [sender_data, set_sender_data] = useState({
     email_address: "",
-    name: "",
+    first_name: "",
+    last_name: "",
     phone_number: "",
     description: "",
   })
+
+  const handleChange = event => {
+    const value = event.target.value
+    set_sender_data({ ...sender_data, [event.target.name]: value })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    alert(
+      "A form was submitted by: " +
+        sender_data.first_name +
+        " " +
+        sender_data.last_name
+    )
+    console.log(sender_data)
+  }
+
+  useEffect(() => {
+    console.log(sender_data)
+  }, [sender_data])
 
   return (
     <div>
       <h1 class="header">Contact</h1>
       <div>
-        <form class="form">
-          <div class="form_child_container"></div>
-          <div class="form_child_container"></div>
-          <div class="form_child_container"></div>
-          <div class="form_child_container"></div>
-          <label class="form_label">Email Address</label>
-          <input type="text" name="email_address" class="form_input" />
-          <label class="form_label">
-            Name
-            <input type="text" name="name" class="form_input" />
-          </label>
-          <label class="form_label">
-            Phone Number
-            <input type="text" name="phone_number" class="form_input" />
-          </label>
-          <label class="form_label">
-            Description
-            <input type="text" name="description" class="form_input" />
-          </label>
-          <button type="submit" class="button_style_one">
-            Send
-          </button>
+        <form class="form" onSubmit={handleSubmit}>
+          <div class="form_child_container">
+            <label class="form_label">Email Address</label>
+            <input
+              type="text"
+              name="email_address"
+              class="form_input"
+              value={sender_data.email_address}
+              onChange={handleChange}
+            />
+          </div>
+          <div class="form_child_container">
+            <label class="form_label">First Name</label>
+            <input
+              type="text"
+              name="first_name"
+              class="form_input"
+              value={sender_data.first_name}
+              onChange={handleChange}
+            />
+          </div>
+          <div class="form_child_container">
+            <label class="form_label">Last Name</label>
+            <input
+              type="text"
+              name="last_name"
+              class="form_input"
+              value={sender_data.last_name}
+              onChange={handleChange}
+            />
+          </div>
+          <div class="form_child_container">
+            <label class="form_label">Phone Number</label>
+            <input
+              type="text"
+              name="phone_number"
+              class="form_input"
+              value={sender_data.phone_number}
+              onChange={handleChange}
+            />
+          </div>
+          <div class="form_child_container">
+            <label class="form_label">Description</label>
+            <input
+              type="text"
+              name="description"
+              class="form_input"
+              value={sender_data.description}
+              onChange={handleChange}
+            />
+          </div>
+          <input class="button_style_one" value="Send" type="submit" />
         </form>
       </div>
     </div>
