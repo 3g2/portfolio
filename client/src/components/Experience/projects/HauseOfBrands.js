39a8ed22b1b18bbc26e53c4abc95538e2318aba4
mohur <m.ashrahfi@gmail.com>
2021-08-10 16:21:01 -0700

    Add basic styles for project card

---
+++ client/src/components/Experience/projects/HauseOfBrands.js
@@ -0,0 +1,40 @@
+import React from "react"
+import Card from "../../utils/Card"
+
+import { hause_of_brands_images } from "../../Consts/SliderData"
+
+const HauseOfBrands = () => {
+  return (
+    <div>
+      <Card
+        slides={hause_of_brands_images}
+        title="Hause of Brands"
+        description="Streamline whole selling vehicles to maximize speed, efficiency, and profits."
+        technologies={[
+          "MongoDB",
+          "Express",
+          "React",
+          "NodeJS",
+          "Material UI",
+          "AWS",
+          "Docker",
+        ]}
+      />
+      <div>
+        <p class="span">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default HauseOfBrands
--- client/src/components/utils/Card.js
+++ client/src/components/utils/Card.js
@@ -4,16 +4,25 @@ import ImageSlider from "./ImageSlider"
 const Card = props => {
   return (
     <div className="card">
-      <ImageSlider slides={props.slides} />
-      <h2 className="card_title">{props.title}</h2>
-      <p className="card_description">{props.description}</p>
-      <button className="button_style_one">More</button>
-      <div className="card_technologies">
-        {props.technologies.map(technology => (
-          <div>{technology}</div>
-        ))}
+      <div></div>
+      <div className="card_image_container">
+        <ImageSlider slides={props.slides} />
       </div>
-      <p></p>
+
+      <div className="card_info_container">
+        <div>
+          <h2 className="card_title">{props.title}</h2>
+          <p className="card_description">{props.description}</p>
+          <button className="button_style_one">More</button>
+        </div>
+        <div className="card_info_container_div_two">
+          <div className="card_technologies">
+            {props.technologies.map(technology => (
+              <div className="card_technologies_span">{technology}</div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
