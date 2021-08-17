23f0c7194c6e805865ea9a44151c3c5044989b97
mohur <m.ashrahfi@gmail.com>
2021-08-16 22:46:39 -0700

    Fix image slider

--- client/src/components/utils/ImageSlider.js
+++ client/src/components/utils/ImageSlider.js
@@ -13,76 +13,158 @@ const ImageSlider = props => {
   //   set_current_img(current_img === 0 ? slide_length - 1 : current_img - 1)
   // }
 
-  const [initial_x, set_initial_x] = useState(0)
-  const [x, set_x] = useState(0)
-  const [pressed, set_pressed] = useState(false)
-  const [slider_inner_offset, set_slider_inner_offset] = useState(0)
+  const [slider_props, set_slider_props] = useState({
+    is_pressed: false,
+    initial_x: null,
+    transform_left_offset: null,
+  })
+
+  const slider_settings = {
+    drag_speed: 1.25,
+    item_width: 300,
+    item_height: 180,
+    item_side_offsets: 15,
+  }
+
+  const sRef = React.createRef()
 
   let slider = document.querySelector(".slider_container")
   let slider_inner = document.querySelector(".slider_inner", null)
 
-  useEffect(() => {
-    return () => {}
-  }, [pressed])
+  const handleParseInt = el => {
+    // extracting 20 from translateX(20px) and converting it to integer with parsInt
+    return parseInt(el.replace("translateX(", "").replace("px)", ""), 10)
+  }
 
-  const handleSliderMouseDown = event => {
-    set_pressed(true)
+  /*
+    The Node.firstChild read-only property returns the node's first child in the 
+    tree,  or null if the node has no children. If the node is a Document, 
+    it returns the first node in the list of its direct children. 
+  */
 
-    set_slider_inner_offset(slider_inner.offsetLeft)
-    set_initial_x(event.nativeEvent.offsetX - slider_inner_offset)
+  const handleMouseDown = event => {
+    const current_ref = sRef.current
+    event.persist()
+    current_ref.classList.add("active")
+    const { initial_x, transform_left_offset } = slider_props
+
+    const temp_initial_x = event.pageX - current_ref.offsetLeft
+    const temp_transform_left_offset = handleParseInt(
+      current_ref.firstChild.style.transform
+    )
+
+    set_slider_props(
+      {
+        is_pressed: true,
+        initial_x: temp_initial_x,
+        transform_left_offset: temp_transform_left_offset,
+      },
+      () => {
+        const x = event.pageX - current_ref.offsetLeft
+        const walk = (x - initial_x) * slider_settings.drag_speed
+
+        current_ref.firstChild.style.cssText = `
+            transform: translateX(${transform_left_offset + walk}px);
+            transition: transform 0.0s ease-in-out;
+          `
+      }
+    )
+
     // slider_inner.style.transform = `translate3d(${x}px, 0px, 0px)`
   }
 
-  const handleSliderMouseUp = event => {
-    set_pressed(false)
-    set_slider_inner_offset(slider_inner.offsetLeft)
-    set_initial_x(event.nativeEvent.offsetX - slider_inner_offset)
-  }
-  const handleSliderMouseEnter = event => {
-    set_pressed(false)
+  const handleMouseUp = event => {
+    handleSnap()
   }
 
-  const handleSliderMouseLeave = event => {
-    set_pressed(false)
+  const handleMouseLeave = event => {
+    handleSnap()
   }
 
-  const handleSliderMouseMove = event => {
-    if (!pressed) return
+  const handleMouseMove = event => {
+    const { is_pressed, initial_x, transform_left_offset } = slider_props
+
+    const current_ref = sRef.current
+
+    if (!is_pressed) return
     event.preventDefault()
 
-    set_x(event.nativeEvent.offsetX)
-    slider_inner.style.left = `${x - initial_x}px`
-    checkBoundary()
+    const x = event.pageX - current_ref.offsetLeft
+
+    const walk = (x - initial_x) * slider_settings.drag_speed
+
+    current_ref.firstChild.style.transform = `translateX(${
+      transform_left_offset + walk
+    }px)`
   }
 
-  const checkBoundary = () => {
-    let outer = slider.getBoundingClientRect()
-    let inner = slider_inner.getBoundingClientRect()
+  const handleSnap = () => {
+    // const { isDown, startX, transLeftOffset } = this.state
+    const current_ref = sRef.current
 
-    if (parseInt(slider_inner.style.left) > 0) {
-      return (slider_inner.style.left = "0px")
-    } else if (inner.right < outer.right) {
-      return (slider_inner.style.left = `-${inner.width - outer.width}px`)
+    // Resetting
+    set_slider_props({ is_pressed: false })
+    current_ref.classList.remove("active")
+
+    // handeling Threshold
+    // (1) getting transValue
+    const temp_threshold_offset = handleParseInt(
+      current_ref.firstChild.style.transform
+    )
+    // (2) items width - 30(first & last item removed margins) - containerWidth(b/c of ending part)
+    const end =
+      props.slides.length *
+        (slider_settings.item_width + 2 * slider_settings.item_side_offsets) -
+      30 -
+      current_ref.offsetWidth
+
+    // (3) check if we passing from threshold ( handeling Snap To Sides )
+    if (temp_threshold_offset < 0 || temp_threshold_offset > end) {
+      set_slider_props({ is_pressed: false })
+      current_ref.firstChild.style.cssText = `
+       transform: translateX(${temp_threshold_offset < 0 ? 0 : end}px);
+       transition: transform 0.5s cubic-bezier(.25,.72,.51,.96);
+     `
     }
+  }
+
+  const sWrapperStyle = {
+    width: `${
+      props.slides.length *
+      (slider_settings.item_width + 2 * slider_settings.item_side_offsets)
+    }px`,
+    height: `${slider_settings.item_height}px`,
+  }
+
+  const itemStyle = {
+    width: `${slider_settings.item_width}px`,
+    height: `${slider_settings.item_height}px`,
+    margin: `0px ${slider_props.item_side_offsets}px`,
   }
 
   return (
     <div
-      className={
-        pressed
-          ? "slider_container slider_container_grabbing"
-          : "slider_container slider_container_grab"
-      }
-      onMouseDown={handleSliderMouseDown}
-      onMouseUp={handleSliderMouseUp}
-      onMouseEnter={handleSliderMouseEnter}
-      onMouseLeave={handleSliderMouseLeave}
-      onMouseMove={handleSliderMouseMove}
+      className="slider_container"
+      ref={sRef}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseLeave}
+      onMouseMove={handleMouseMove}
     >
-      <div className="slider_inner">
+      <div
+        className="slider_inner"
+        style={{
+          ...sWrapperStyle,
+          transform: "translateX(0px)",
+        }}
+      >
         {props.slides.map((slide, index) => {
           return (
-            <div className="slider_image_container">
+            <div
+              className="slider_image_container"
+              key={index}
+              style={{ ...itemStyle }}
+            >
               <img src={slide.image} alt="images" className="slider_image" />
             </div>
           )
