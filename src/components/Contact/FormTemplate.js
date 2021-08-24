8d49c8fe5ae7b38ee72e468fb717d1d8777068b3
mohur <m.ashrahfi@gmail.com>
2021-08-23 23:48:38 -0700

    Update form component styles to being mobile friendly

--- src/components/Contact/FormTemplate.js
+++ src/components/Contact/FormTemplate.js
@@ -25,112 +25,114 @@ const FormTemplate = () => {
       }}
     >
       {({ errors, touched }) => (
-        <Form className="form">
-          <div className="form_child_container">
-            <div className="form_grandchild_container">
-              <div className="form_placement">
-                <div className="form_label">First Name</div>
-                <label>Phone Number</label>
-                <Field
-                  id="phone_number"
-                  type="text"
-                  name="first_name"
-                  className="form_input"
-                />
-                <div className="errors">
-                  {errors.first_name && touched.first_name ? (
-                    <div> {errors.first_name} </div>
-                  ) : null}
+        <div>
+          <Form className="form">
+            <div className="form_child_container">
+              <div className="form_grandchild_container">
+                <div className="form_placement">
+                  <div className="form_label">First Name</div>
+                  <label>Phone Number</label>
+                  <Field
+                    id="phone_number"
+                    type="text"
+                    name="first_name"
+                    className="form_input"
+                  />
+                  <div className="errors">
+                    {errors.first_name && touched.first_name ? (
+                      <div> {errors.first_name} </div>
+                    ) : null}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="form_grandchild_container">
-              <div className="form_placement">
-                <div className="form_label">Last Name</div>
-                <Field
-                  id="email_address"
-                  type="text"
-                  name="last_name"
-                  className="form_input"
-                />
-                <div className="errors">
-                  {errors.last_name && touched.last_name ? (
-                    <div> {errors.last_name} </div>
-                  ) : null}
+              <div className="form_grandchild_container">
+                <div className="form_placement">
+                  <div className="form_label">Last Name</div>
+                  <Field
+                    id="email_address"
+                    type="text"
+                    name="last_name"
+                    className="form_input"
+                  />
+                  <div className="errors">
+                    {errors.last_name && touched.last_name ? (
+                      <div> {errors.last_name} </div>
+                    ) : null}
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-
-          <div className="form_child_container">
-            <div className="form_grandchild_container">
-              <div className="form_placement">
-                <div className="form_label">Email Address</div>
-                <label>Phone Number</label>
-                <Field
-                  id="phone_number"
-                  type="email"
-                  name="email_address"
-                  className="form_input"
-                />
-                <div className="errors">
-                  {errors.email_address && touched.email_address ? (
-                    <div> {errors.email_address} </div>
-                  ) : null}
-                </div>
-              </div>
-            </div>
-            <div className="form_grandchild_container">
-              <div className="form_placement">
-                <div className="form_label">
-                  <label>Email Address</label>
-                  Phone Number • <i className="optional">Optional</i>
-                </div>
-                <Field
-                  id="email_address"
-                  type="text"
-                  name="phone_number"
-                  className="form_input"
-                />
-                <div className="errors">
-                  {errors.phone_number && touched.phone_number ? (
-                    <div> {errors.phone_number} </div>
-                  ) : null}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="form_grandchild_container">
-            <div className="form_placement">
-              <label className="form_label">Description</label>
-              <Field as="textarea" type="text" name="description" />
-              <div className="errors">
-                {errors.description && touched.description ? (
-                  <div> {errors.description} </div>
-                ) : null}
-              </div>
-            </div>
-          </div>
-
-          <div className="form_button_group">
-            <div className="form_button_group_child">
-              <input
-                className="button_style_one form_button"
-                value="Send"
-                type="submit"
-              />
             </div>
 
-            <div className="form_button_group_child">
-              <a
-                className="button_style_one form_email_app_button"
-                href="mailto:m.ashrahfi@gmail.com"
-              >
-                open default email client
-              </a>
+            <div className="form_child_container">
+              <div className="form_grandchild_container">
+                <div className="form_placement">
+                  <div className="form_label">Email Address</div>
+                  <label>Phone Number</label>
+                  <Field
+                    id="phone_number"
+                    type="email"
+                    name="email_address"
+                    className="form_input"
+                  />
+                  <div className="errors">
+                    {errors.email_address && touched.email_address ? (
+                      <div> {errors.email_address} </div>
+                    ) : null}
+                  </div>
+                </div>
+              </div>
+              <div className="form_grandchild_container">
+                <div className="form_placement">
+                  <div className="form_label">
+                    <label>Email Address</label>
+                    Phone Number • <i className="optional">Optional</i>
+                  </div>
+                  <Field
+                    id="email_address"
+                    type="text"
+                    name="phone_number"
+                    className="form_input"
+                  />
+                  <div className="errors">
+                    {errors.phone_number && touched.phone_number ? (
+                      <div> {errors.phone_number} </div>
+                    ) : null}
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </Form>
+            <div className="form_grandchild_container">
+              <div className="form_placement">
+                <div className="form_label">Description</div>
+                <Field as="textarea" type="text" name="description" />
+                <div className="errors">
+                  {errors.description && touched.description ? (
+                    <div> {errors.description} </div>
+                  ) : null}
+                </div>
+              </div>
+            </div>
+
+            <div className="form_button_group">
+              <div className="form_button_group_child">
+                <input
+                  className="button_style_one"
+                  value="Send"
+                  type="submit"
+                />
+              </div>
+
+              <div className="form_button_group_child">
+                <a
+                  className="button_style_one"
+                  href="mailto:m.ashrahfi@gmail.com"
+                >
+                  use email client instead
+                </a>
+              </div>
+            </div>
+          </Form>
+        </div>
       )}
     </Formik>
   )
