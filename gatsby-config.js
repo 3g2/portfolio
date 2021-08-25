2ed41f226dad03a5536ca2da0150f794a8d559ac
mohur <m.ashrahfi@gmail.com>
2021-08-25 15:41:44 -0700

    Add react-helmet to gatsby-config for CSP to work & fix meta typos

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
 module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
