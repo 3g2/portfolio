4d37213cfa9b75e17aad90c333396578dad8907d
mohur <m.ashrahfi@gmail.com>
2021-08-26 20:16:21 -0700

    Test Content Security Policy

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,6 @@ require("dotenv").config({
 
 module.exports = {
   pathPrefix: "/portfolio/client",
-}
-
-module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -1,13 +1,13 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
 
-const Index = () => {
+const Metadata = () => {
   return (
     <div>
       <Helmet>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
         <meta
           http-equiv="Content-Security-Policy"
           content="
@@ -18,14 +18,12 @@ const Index = () => {
             img-src 'self' data: content:;
           "
         />
-
         <meta
           //DESCRIPTION
           name="description"
           content="College student who strives in improving on collaboration, problem
           solving, and designing/implementing for programming and applications."
         />
-
         <meta name="keywords" content="" />
         <title>Maher Halabi - Software Engineer</title>
         <link rel="canonical" />
@@ -34,4 +32,33 @@ const Index = () => {
   )
 }
 
-export default Index
+// const Metadata = ({ title, description, url, charset }) => {
+//   const data = useStaticQuery(
+//     graphql`
+//       query {
+//         site {
+//           siteMetadata {
+//             charset
+//             title
+//             description
+//             url
+//           }
+//         }
+//       }
+//     `
+//   )
+//   const metaTitle = title
+//   const metaDescription = description
+//   const metaUrl = url
+//   const metaCharset = charset
+//   return (
+//     <Helmet>
+//       <title>{`${metaTitle}`}</title>
+//       <meta name="description" content={metaDescription} />
+//       <meta name="url" content={metaUrl} />
+//       <meta charset={metaCharset} />
+//     </Helmet>
+//   )
+// }
+
+export default Metadata
