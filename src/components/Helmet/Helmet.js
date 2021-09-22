b95d00d3f8930d8c19a8b33c9568fc011bff70f9
mohur <m.ashrahfi@gmail.com>
2021-09-21 20:04:59 -0700

    Fix sending contact info for forms

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -11,7 +11,7 @@ const Metadata = () => {
         <meta
           http-equiv="Content-Security-Policy"
           content="
-          default-src 'self'; 
+          default-src 'self' https://api.emailjs.com/api/v1.0/email/send; 
           font-src http://fonts.gstatic.com; 
           style-src 'self' 'unsafe-inline' http://fonts.googleapis.com;
           "
