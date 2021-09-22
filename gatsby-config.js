5250985a4514bfb10e2b5204a7391d40ac5989c3
mohur <m.ashrahfi@gmail.com>
2021-09-21 22:14:36 -0700

    Fix Gatsby config env for google analytics trackingID

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,16 +9,21 @@ module.exports = {
     title: "Maher Halabi - Software Engineer",
   },
   plugins: [
-    // {
-    //   resolve: `gatsby-plugin-google-gtag`,
-    //   options: {
-    //     trackingIds: `"${process.env.GATSBY_GOOGLE_TRACKING_ID}"`,
-    //     pluginConfig: {
-    //       head: true,
-    //       anonymize_ip: true,
-    //     },
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-9EP8W5EB0H", // Google Analytics / GA
+        ],
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+        pluginConfig: {
+          head: true,
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     {
